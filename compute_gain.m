0bdb8338fb77cda5c4713837ec9e5126a02f70ce
Dongchan Lee <dclee131@users.noreply.github.com>
2017-10-06 13:22:52 -0400

    code update

--- compute_gain.m
+++ compute_gain.m
@@ -1,6 +1,6 @@
 %% Settings
 %clear; close all;
-if ~exist('sys_case','var'); sys_case=39; end % default IEEE system
+if ~exist('sys_case','var'); sys_case=9; end % default IEEE system
 
 run(['dyn' int2str(sys_case)]) % Get info from data file
 slack_bus=SW.con(1);
@@ -8,8 +8,9 @@ num_bus=size(Bus.con,1);
 num_line=size(Line.con,1);
 num_gen=size(Syn.con,1);
 num_load=num_bus-num_gen;
+num_var=num_bus+num_gen;
 
-eye_bus=eye(num_bus); idx_gen=setdiff(Syn.con(:,1),slack_bus); idx_load=setdiff(Bus.con(:,1),Syn.con(:,1));
+eye_bus=eye(num_bus); idx_gen=Syn.con(:,1); idx_load=setdiff(Bus.con(:,1),Syn.con(:,1));
 
 Syn.con(:,19)=2*ones(size(Syn.con(:,19))); % Add damping
 Syn.con(:,4)=10;
@@ -33,13 +34,13 @@ V_eq=x_eq(num_bus+1:end).*(cos(x_eq(1:num_bus))+1i*sin(x_eq(1:num_bus)));
 I_eq=Y*V_eq;
 S_inj=V_eq.*conj(I_eq);
 P_inj=real(S_inj);
-P_G=P_inj(idx_gen);
+P_G=P_inj(idx_gen); P_G(slack_bus)=-sum(P_inj(setdiff(Bus.con(:,1),slack_bus)));
 P_L=P_inj(idx_load);
 %P_inj=E'*diag(B_line)*sin(E*delta_eq);
 
-num_gen=num_gen-1; num_bus=num_bus-1; idx_bus=setdiff(Bus.con(:,1),slack_bus);
+%num_gen=num_gen-1; num_bus=num_bus-1; idx_bus=setdiff(Bus.con(:,1),slack_bus);
+%M=M(Syn.con(:,1)~=slack_bus); D_G=D_G(Syn.con(:,1)~=slack_bus);
 E_G=E(:,idx_gen); E_L=E(:,idx_load); E=[E_G E_L];
-M=M(Syn.con(:,1)~=slack_bus); D_G=D_G(Syn.con(:,1)~=slack_bus);
 idx_delta_gen=1:num_gen; idx_omega=num_gen+1:2*num_gen; idx_delta_load=2*num_gen+1:num_bus+num_gen;
 idx_delta=[1:num_gen 2*num_gen+1:num_bus+num_gen];
 
@@ -51,7 +52,7 @@ J_sys=@(x) [zeros(num_gen) eye(num_gen) zeros(num_gen,num_load);
     -diag(1./M)*E_G'*diag(B_line)*diag(cos(E*x(idx_delta)))*E_G -diag(D_G./M) -diag(1./M)*E_G'*diag(B_line)*diag(cos(E*x(idx_delta)))*E_L;
     -diag(1./D_L)*E_L'*diag(B_line)*diag(cos(E*x(idx_delta)))*E_G zeros(num_load,num_gen) -diag(1./D_L)*E_L'*diag(B_line)*diag(cos(E*x(idx_delta)))*E_L];
 
-delta0=(E'*diag(B_line)*E)\[P_G; P_L];
+delta0=[0; (E(:,2:end)'*diag(B_line)*E(:,2:end))\[P_G(2:end); P_L]];
 x0=[delta0(1:num_gen); zeros(num_gen,1); delta0(num_gen+1:num_bus)];
 
 % Jacobian check
@@ -62,23 +63,27 @@ for i=1:size(x,1)
 end
 J_fail=max(max(abs(J_sys(x)-J_check)));
 
-x_eq = NR(f_sys,J_sys,x0);
+f_eq=@(x) [f_sys(x); x(1)];
+J_eq=@(x) [J_sys(x); 1 zeros(1,num_var-1)];
+x_eq = NR(f_eq,J_eq,x0);
 if max(x_eq)>1e3; return; end
 
 %% Formulate the model
 A=[zeros(num_gen) eye(num_gen) zeros(num_gen,num_load);
     -diag(M.^-1)*E_G'*diag(B_line)*diag(cos(E*x_eq(idx_delta)))*E_G -diag(D_G./M) -diag(M.^-1)*E_G'*diag(B_line)*diag(cos(E*x_eq(idx_delta)))*E_L;
     -diag(D_L.^-1)*E_L'*diag(B_line)*diag(cos(E*x_eq(idx_delta)))*E_G zeros(num_load,num_gen) -diag(D_L.^-1)*E_L'*diag(B_line)*diag(cos(E*x_eq(idx_delta)))*E_L];
-B=[zeros(num_gen,num_bus); diag(M.^-1) zeros(num_gen, num_load); zeros(num_load, num_gen) diag(D_L.^-1)];
+B_u=[zeros(num_gen,num_bus); diag(M.^-1) zeros(num_gen, num_load); zeros(num_load, num_gen) diag(D_L.^-1)];
 B_v=[zeros(num_gen,num_line); -diag(M.^-1)*E_G'*diag(B_line); -diag(D_L.^-1)*E_L'*diag(B_line)];
+C_y=[zeros(num_gen) eye(num_gen) zeros(num_gen,num_load)];
+C_w=[E_G zeros(num_line,num_gen) E_L];
 
-G{1}=ss(A,B,eye(num_bus+num_gen),zeros(num_bus+num_gen,num_bus));
-G{2}=ss(A,B_v,eye(num_bus+num_gen),zeros(num_bus+num_gen,num_line));
-G{3}=ss(A,B,[E_G zeros(num_line,num_gen) E_L],zeros(num_line,num_bus));
-G{4}=ss(A,B_v,[E_G zeros(num_line,num_gen) E_L],zeros(num_line));
+G{1}=ss(A,B_u,C_y,zeros(num_gen,num_bus));
+G{2}=ss(A,B_v,C_y,zeros(num_gen,num_line));
+G{3}=ss(A,B_u,C_w,zeros(num_line,num_bus));
+G{4}=ss(A,B_v,C_w,zeros(num_line));
 
 %% Compute Infinity gains
-method=1; % 1: Direct impulse computation / 2: using / 3: both
+method=2; % 1: Direct impulse computation / 2: using / 3: both
 
 if method==1 || method==3 % compute with direct integration
     tic;
@@ -86,7 +91,8 @@ if method==1 || method==3 % compute with direct integration
         sys=G{idx_io};
         for i=1:size(sys,1)
             for j=1:size(sys,2)
-                [y_impulse,t_impulse]=impulse(sys(i,j));
+                [y_impulse,t_impulse]=impulse(sys(i,j),10);
+                if abs(y_impulse(end))>1; disp(['Gain not converged!' num2str(y_impulse(end))]); return; end;
                 gain_mtx_direct{idx_io}(i,j)=trapz(t_impulse,abs(y_impulse));
             end
         end
@@ -100,6 +106,7 @@ if method==2 || method==3
     tic;
     [num,den] = tfdata(G{1}(1,1));
     sys_order=size(den{1},2);
+    [r,p,k] = residue(num{1},den{1});
     figure; hold all;
     for idx_order=1:sys_order
         num=zeros(1,sys_order); num(idx_order)=1;
@@ -121,3 +128,6 @@ if method==2 || method==3
     disp(['Computation time for direct approach: ' num2str(cp_improv)])
 end
 if method==3; max(max(abs((gain_mtx_direct{1}-gain_mtx{1})))); end
+
+% Test eigenvalue w.r.t. system size
+%n_rng=1:100; for n=n_rng; eig_size(n)=min(real(eig(eye(n)-0.1*ones(n,n)))); end; plot(n_rng,eig_size)
