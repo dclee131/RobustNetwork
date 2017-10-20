61296e15f86b71492fb7baecc29314dce012d9b4
Dongchan Lee <dclee131@users.noreply.github.com>
2017-10-19 22:23:00 -0400

    network visualization

--- src/core/optimize_bound.jl
+++ src/core/optimize_bound.jl
@@ -3,21 +3,25 @@ using Ipopt
 using JuMP
 
 # Setting
-sys_case=9;
-pert_bus=6;
+sys_case=39;
+loading_level=100;
+pert_bus=1;
+compute_gain_method=2; # 1: uses direct simulation, 2: load precomputed gain
 
 ###### Begin Code #######
 # Set up model and compute gain
 mat"""
 addpath(genpath(pwd))
 sys_case=$sys_case;
-method=1;
+compute_gain_method=$compute_gain_method;
+loading_level=$loading_level;
 compute_gain;
 $gain_mtx=gain_mtx;
 $w_0=E*delta0;
 $num_line=num_line;
 $num_bus=num_bus;
 $idx_gen=idx_gen;
+$cp_gain=cp_direct;
 """
 size_w=convert(Int64,num_line);
 size_u=convert(Int64,num_bus);
@@ -34,12 +38,19 @@ m = Model(solver=IpoptSolver());
 @constraint(m,gain_mtx[3]*u.<=(eye(size_w)-gain_mtx[4]*diagm(cos.(w_0)))*w-gain_mtx[4]*sin.(abs.(w_0))+gain_mtx[4]*sinw)
 @objective(m, Max, c'*u)
 solve(m)
-w_bar=getvalue(w); u_bar=getvalue(u);
+w_bar=getvalue(w); u_bar=getvalue(u); sinw_bar=getvalue(sinw);
 println("w = ", w_bar); println("u = ", u_bar);
 
 # Visualize result on the network
 mat"""
-if sum(sys_case==[9 39])
+if sum(sys_case==[9 14 39])
  close all; VisualizeNetwork(sys_case,$u_bar,$w_bar);
 end
+sgt_mtx=eye($size_w)-gain_mtx{4}*diag(cos($w_0)+sin(abs($w_0))-$sinw_bar);
+figure; imagesc(sgt_mtx); colorbar;
 """
+
+println("Result Summary")
+println("Maximum perturbation: ", w_bar[pert_bus], " p.u.")
+println("Gain Computation Time: ", cp_gain, " seconds")
+println("IPOPT Optimization Time: ", cp_gain, " seconds")
--- src/plot/VisualizeNetwork.m
+++ src/plot/VisualizeNetwork.m
@@ -8,6 +8,9 @@ if sys_case==9
     NodeCoordinates=[40 10; 5 40; 75 40; 40 20; 30 30; 50 30; 20 40; 40 40; 60 40];
     EdgeConnections=[ 9 8; 7 8; 9 6; 7 5; 5 4; 6 4; 2 7; 3 9; 1 4];
     axis([0 80 0 50])
+elseif sys_case==14
+    NodeCoordinates=[10 50; 15 10; 75 13; 75 55; 20 35; 25 55; 82 66; 90 70; 72 72; 50 70; 38 75; 15 85; 30 90; 50 85];
+    EdgeConnections=[2 5; 6 12;12 13; 6 13; 6 11; 11 10; 9 10; 9 14; 14 13; 7 9; 1 2; 3 2; 3 4; 1 5; 5 4; 2 4; 5 6; 4 9; 4 7; 8 7];
 elseif sys_case==39
     NodeCoordinates=[25 70; 30 80; 30 70; 35 60; 30 50; 30 20; 25 30; 25 40; 20 50; 45 25; 35 35; 45 45; 55 35; 55 60;
     62.5 65; 70 70; 50 70; 40 70; 70 40; 60 25; 85 60; 85 50; 80 20; 75 50; 40 90; 50 90; 50 80; 65 80; 80 90; 30 100;
@@ -36,7 +39,7 @@ end
 t=0:2*pi/20:2*pi;
 for i = 1:size(NodalValues,1)
    center = [NodeCoordinates(i,1) NodeCoordinates(i,2)];
-   radius = abs(NodalValues(i))*1;
+   radius = abs(NodalValues(i))*1+0.5;
    if NodalValues(i) > 0
        fill3(sin(t)*radius+center(1),cos(t)*radius+center(2),zeros(size(t))+height,'b');
    elseif NodalValues(i) < 0
